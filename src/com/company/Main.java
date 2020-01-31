34cf55ae5bbbcbdb12283d2bc6116927f27740cc
GBTC441010UR <GBTC441010UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-01-31 09:06:39 -0500

    Morning Commit: Nothing to See Yet

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+/*
+
+
+
+Program: Assemble 25 Burritos
+
+i. Random generator for each burrito option: min 5, max 9
+
+ii. Save and Display burritos
+
+iii. Display price of each burrito
+a. $3.00 base price, tortilla
+b. $0.50 for each additional burrito
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
